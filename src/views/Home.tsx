2da636425b53e0ccfefefd321bf2a8f09bc3f867
Irvin Delgado <irvind21@gmail.com>
2022-04-28 23:44:03 -0500

    basic sorting animation

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,49 +1,77 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSprings, animated } from 'react-spring';
 import '../styles/main.css'
 
 
-const algoName = 'Bubble Sort';
-const algoCode = 
-`
-def BubbleSort(elements):
-  # Looping from size of array from last index[-1] to index [0]
-  for n in range(len(elements)-1, 0, -1):
-    for i in range(n):
-      if elements[i] > elements[i + 1]:
-        # swapping data if the element is less than next element in the array
-        elements[i], elements[i + 1] = elements[i + 1], elements[i]
-`;
-const numArray = [2, 3, 1, 6, 5, 10, 7, 9, 4, 8];
+
 const Home = () => {
-  const [arr, setArr] = useState(numArray);
-  const idxSmall = useRef(0);
+  const algoName = 'Bubble Sort';
+  const algoCode = 
+  `
+  def BubbleSort(elements):
+    # Looping from size of array from last index[-1] to index [0]
+    for n in range(len(elements)-1, 0, -1):
+      for i in range(n):
+        if elements[i] > elements[i + 1]:
+          # swapping data if the element is less than next element in the array
+          elements[i], elements[i + 1] = elements[i + 1], elements[i]
+  `;
+  const NUM_ARR = [2, 3, 1, 6, 5, 10, 7, 9, 4, 8];
+  const [order, setOrder] = useState(NUM_ARR);
+  const left = useRef(0);
+  const right = useRef(1);
+
+  const animationFunctions = (idx:number) => {
+    switch (idx) {
+      case left.current:
+      case right.current:
+        return {
+          backgroundColor: '#46e891',
+        }
+      default:
+        return {
+          backgroundColor: '#F4F2F3',
+        }
+    }
+  }
+
+  const [springs, api] = useSprings(order.length, (idx) => animationFunctions(idx));
+
+  const swap = (arr: number[], leftIdx:number, rightIdx:number) => {
+    const newArr = [...arr];
+    if(newArr[leftIdx]>newArr[rightIdx])
+      [newArr[leftIdx], newArr[rightIdx]] = [newArr[rightIdx], newArr[leftIdx]];
+    return newArr
+  }
+
   const swapArr = () => {
-    setArr((prevArr) => {
-      const newArr = [...prevArr];
-      [newArr[0],newArr[1]]=[newArr[1],newArr[0]];
-      return newArr;
-    });
+    setOrder((prevState)=>swap(prevState,left.current,right.current));
+    if(right.current===order.length-1) {
+      left.current = 0;
+      right.current = 1; 
+      return
+    }
+    left.current++;
+    right.current++;
   };
-  const springs = useSprings(
-    arr.length, 
-    arr.map((item,idx) => ({
-      to: {
-        height: (item*35),
-        background: idx===idxSmall.current ?'red':'#F4F2F3',
-      },
-    }))
-  );
+  
+  useEffect(()=>{
+    api.start(index=>(animationFunctions(index)));
+  })
   
   return (
     <div>
       <div className='algoName'>{algoName}</div>
+      
       <button onClick={swapArr}>SWAP</button>
       <div className='algoViz'>
         {springs.map((animation,indx) => (
-          <animated.div key={indx} style={animation} className="bar" />
-          )
-        )}
+          <animated.div key={indx} className="bar" style={{
+            ...animation, 
+            height:order[indx]*35,
+            y:0,
+          }}/>
+        ))}
       </div>
       <div className='algoCode'>
         <pre>
