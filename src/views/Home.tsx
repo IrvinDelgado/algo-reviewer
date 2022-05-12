c7069a9749e11be62f478003999c0cc31b977088
Irvin Delgado <irvind21@gmail.com>
2022-05-12 16:12:07 -0500

    routing foundation

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,13 +1,33 @@
-// import BubbleSort from "../components/algo/bubble-sort/BubbleSort";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+} from "react-router-dom";
+import BubbleSort from "../components/algo/bubble-sort/BubbleSort";
 import SelectionSort from "../components/algo/selection-sort/SelectionSort";
 
 const Home = () => {
-
   return (
-    <div>
-      {/* <BubbleSort/> */}
-      <SelectionSort/>
-    </div>
+    <Router>
+      <nav className="nav">
+        <ul>
+          <li>
+            <Link to="/bubble">Bubble</Link>
+          </li>
+          <li>
+            <Link to="/selection">Selection</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="content">
+        <Routes>
+          <Route path="/bubble" element={<BubbleSort/>}/>
+          <Route path="/selection" element={<SelectionSort/>}/>
+          <Route path="*" element={<BubbleSort/>}/>
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
