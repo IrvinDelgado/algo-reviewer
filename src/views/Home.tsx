d2c6176d29f15a354a4316595895ea8a32ad8cba
Irvin Delgado <irvind21@gmail.com>
2022-05-13 15:06:19 -0500

    simple nav

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -10,22 +10,18 @@ import SelectionSort from "../components/algo/selection-sort/SelectionSort";
 const Home = () => {
   return (
     <Router>
-      <nav className="nav">
-        <ul>
-          <li>
-            <Link to="/bubble">Bubble</Link>
-          </li>
-          <li>
-            <Link to="/selection">Selection</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="content">
-        <Routes>
-          <Route path="/bubble" element={<BubbleSort/>}/>
-          <Route path="/selection" element={<SelectionSort/>}/>
-          <Route path="*" element={<BubbleSort/>}/>
-        </Routes>
+      <div className="page">
+        <div className="nav">
+          <Link className="navItem" to="/bubble">Bubble</Link>      
+          <Link className="navItem" to="/selection">Selection</Link>  
+        </div>
+        <div className="content">
+          <Routes>
+            <Route path="/bubble" element={<BubbleSort/>}/>
+            <Route path="/selection" element={<SelectionSort/>}/>
+            <Route path="*" element={<BubbleSort/>}/>
+          </Routes>
+        </div>
       </div>
     </Router>
   )
